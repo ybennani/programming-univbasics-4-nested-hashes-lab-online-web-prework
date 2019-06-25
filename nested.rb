0d2735572aae19abf95ead98f18eaf2604ba1812
yassir bennani <ybennani@yahoo.com>
2019-06-25 21:57:25 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -20,7 +20,7 @@ def hopper
 end
 
 def alan_kay_is_known_for
-	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
+	
 	 programmer_hash[:alan_kay][:known_for]
 
 
@@ -62,15 +62,10 @@ end
 def adding_matz
  programmer_hash[:yukihiro_matsumoto] ={:known_for => "Ruby",:languages => ["LISP, C"]}
 
+  programmer_hash
 
 
 
-
-# add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#    :languages => ["LISP", "C"]
-# }
 
 	programmer_hash =
  		{
@@ -107,7 +102,7 @@ def changing_alan
           :languages => ["C"]
         }
      }
-     #change what Alan Kay is :known_for to the value of the alans_new_info variable.
+    
      alans_new_info = "GUI"
 		 programmer_hash[:alan_kay][:known_for] = alans_new_info
      	  programmer_hash
