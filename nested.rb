0eb81dcfee0b4e77e56835fa9b37bc0a1134d1b1
yassir bennani <ybennani@yahoo.com>
2019-06-25 21:57:27 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -20,7 +20,7 @@ def hopper
 end
 
 def alan_kay_is_known_for
-	
+
 	 programmer_hash[:alan_kay][:known_for]
 
 
@@ -102,7 +102,7 @@ def changing_alan
           :languages => ["C"]
         }
      }
-    
+
      alans_new_info = "GUI"
 		 programmer_hash[:alan_kay][:known_for] = alans_new_info
      	  programmer_hash
